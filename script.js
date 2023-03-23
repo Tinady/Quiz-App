b30d0307977758f3f98121bbbe47998ec872f115
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2023-03-23 21:04:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const  quizData= [
+    {
+        question:"How old are u?",
+        a:19,
+        b:29,
+        c:39,
+        d:49,
+         },
+    
+         {
+            question:"What is your favourite coloe",
+            a:'green',
+            b:'blue',
+            c:'yellow',
+            d:'pink',
+             },
 
+       {
+                question:"what is my name",
+                 a:'green',
+                 b:'blue',
+                 c:'yellow',
+                 d:'Brown',
+             },
+                
+]
